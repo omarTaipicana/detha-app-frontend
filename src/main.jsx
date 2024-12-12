a653f87711a447f9018b93dc5722a3f523c04a6b
Fernando <fernandoparadoja@gmail.com>
2024-12-12 15:45:17 -0500

    rutasss

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+    <HashRouter basename="/" future={{ v7_relativeSplatPath: true }}>
         <App />
       </HashRouter>
     </Provider>
